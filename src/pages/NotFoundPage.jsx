01aab651b30e8215f25d03823f1ef5f6a3933ea8
jsmolos <molosj@yahoo.com>
2024-05-03 01:04:52 +0800

    lazily

--- src/pages/NotFoundPage.jsx
+++ src/pages/NotFoundPage.jsx
@@ -1,20 +1,7 @@
 import React from 'react';
-import { useNavigate } from 'react-router-dom';
 
 const NotFoundPage = () => {
-  const navigate = useNavigate();
-
-  const handleHomeClick = () => {
-    navigate('/');
-  };
-
-  return (
-    <div>
-      <h1>404 - Page Not Found</h1>
-      <p>The page you are looking for might have been removed or is temporarily unavailable.</p>
-      <button onClick={handleHomeClick}>Go to Home</button>
-    </div>
-  );
+  return <div>NotFoundPage</div>;
 };
 
 export default NotFoundPage;
